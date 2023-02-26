c5d191a5552745a0647a3d4823bbc69af28dc942
Sown13 <sonnh.buh@gmail.com>
2023-02-26 17:11:23 +0700

    exercise 2,3,4

--- calculator.js
+++ calculator.js
@@ -1,29 +1,29 @@
 
 function addition(){
     let x,y,a;
-    x = document.getElementById("number1").valueAsNumber;
-    y = document.getElementById("number2").valueAsNumber;
+    x = parseInt(document.getElementById("number1").valueAsNumber);
+    y = parseInt(document.getElementById("number2").valueAsNumber);
     a = x + y ;
     document.getElementById("result").innerHTML = "Result: total =" + " " + a ;
 }
 function subtraction() {
     let x,y,b;
-    x = document.getElementById("number1").valueAsNumber;
-    y = document.getElementById("number2").valueAsNumber;
+    x = parseInt(document.getElementById("number1").valueAsNumber);
+    y = parseInt(document.getElementById("number2").valueAsNumber);
     b = x - y ;
     document.getElementById("result").innerHTML = "Result: Subtraction =" + " " + b ;
 }
 function multiple() {
     let x,y,c;
-    x = document.getElementById("number1").valueAsNumber;
-    y = document.getElementById("number2").valueAsNumber;
+    x = parseInt(document.getElementById("number1").valueAsNumber);
+    y = parseInt(document.getElementById("number2").valueAsNumber);
     c = x*y ;
     document.getElementById("result").innerHTML = "Result: Multiple =" + " " + c ;
 }
 function division() {
     let x,y,d;
-    x = document.getElementById("number1").valueAsNumber;
-    y = document.getElementById("number2").valueAsNumber;
+    x = parseInt(document.getElementById("number1").valueAsNumber);
+    y = parseInt(document.getElementById("number2").valueAsNumber);
     d = x/y ;
     document.getElementById("result").innerHTML = "Result: Division =" + " " + d ;
 }
